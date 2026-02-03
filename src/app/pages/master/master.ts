bfab7d562ddd1f66dc8047a7e7433965bccbffdf
Mahender <mahenderktl750@gmail.com>
2026-02-03 21:37:58 +0530

    Added Cards with Data in Master Page

--- src/app/pages/master/master.ts
+++ src/app/pages/master/master.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../core/services/master/master-service';
+import { IMaster } from '../../core/modal/interfaces/IMaster';
 
 @Component({
   selector: 'app-master',
@@ -7,6 +8,47 @@ import { MasterService } from '../../core/services/master/master-service';
   templateUrl: './master.html',
   styleUrl: './master.css',
 })
-export class Master {
+export class Master implements OnInit {
   masterService = inject(MasterService);
+
+  masterDataList: IMaster[] = [];
+  filteredMasterData: IMaster[] = [];
+
+  icons!: Map<string, string>;
+
+  constructor() {
+    this.getIcons();
+  }
+
+  ngOnInit(): void {
+    this.getAllMasterDataList();
+  }
+  getAllMasterDataList() {
+    const loadMasterSub$ = this.masterService.getAllMasterData().subscribe({
+      next: (res: any) => {
+        this.getAllMasterDataWithIcons(res.data);
+      },
+    });
+    /* this.subscriptionList.push(loadMasterSub$); */
+  }
+  getAllMasterDataWithIcons(res: IMaster[]) {
+    this.masterDataList = res.filter((master: IMaster) =>
+      this.icons.has(master.masterValue.toUpperCase()),
+    );
+    console.log('Data :' + this.masterDataList);
+
+    this.filteredMasterData = [...this.masterDataList];
+  }
+  getIcons() {
+    this.icons = new Map<string, string>([
+      ['CASH', 'bi-cash-coin'],
+      ['CARD', 'bi-credit-card'],
+      ['UPI', 'bi-phone'],
+      ['INVOICE', 'bi-receipt'],
+      ['RECEIPT', 'bi-file-earmark-check'],
+      ['VOUCHER', 'bi-ticket'],
+      ['NET BANKING', 'bi-bank'],
+      ['CHEQUE', 'bi-file-earmark'],
+    ]);
+  }
 }
